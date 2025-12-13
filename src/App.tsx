d12d89836223806c9fbc37a143f2abc43cb1ba57
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-12-12 22:32:46 -0300

    feat: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./components/custom/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <h2>Hello World</h2>
+      <Header />
     </div>
   );
 }
