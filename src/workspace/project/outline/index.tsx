f231bbbbd9e78f250c9bac975fe75d08d9f73dab
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-12-23 20:52:34 -0300

    feat: Outline GetProjectDetail

--- src/workspace/project/outline/index.tsx
+++ src/workspace/project/outline/index.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { firebaseDb } from "../../../../config/FirebaseConfig";
+import { doc, getDoc } from "firebase/firestore";
+import { useParams } from "react-router-dom";
 
 type Props = {};
 
-const index = (props: Props) => {
-  return <div>index</div>;
+type Project = {
+  userInputPrompt: string;
+  projectId: string;
+  createdAt: string;
 };
 
-export default index;
+const Outline = (props: Props) => {
+  const { projectId } = useParams();
+  const [projectDetail, setProjectDetail] = useState<Project>();
+
+  const GetProjectDetail = async () => {
+    const docRef = doc(firebaseDb, "projects", projectId ?? "");
+    const docSnap = await getDoc(docRef);
+    if (!docSnap.exists()) return;
+    setProjectDetail(docSnap.data() as Project);
+  };
+
+  useEffect(() => {
+    if (!projectId) return;
+    (async () => {
+      await GetProjectDetail();
+    })();
+  }, [projectId]);
+
+  return <div>Outline</div>;
+};
+
+export default Outline;
