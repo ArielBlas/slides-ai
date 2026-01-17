464031aa1db2345fd27378b97fc5bb5d97e951db
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2026-01-16 23:41:27 -0300

    feat: header access

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -1,10 +1,11 @@
 import React, { useContext } from "react";
 import logo from "../../assets/logo.png";
 import { Button } from "../ui/button";
-import { SignInButton, UserButton, useUser } from "@clerk/clerk-react";
+import { SignInButton, useAuth, UserButton, useUser } from "@clerk/clerk-react";
 import { Link, useLocation } from "react-router-dom";
 import { Gem } from "lucide-react";
 import { UserDetailContext } from "../../../context/UserDetailContext";
+import { Link } from "react-router-dom";
 
 type Props = {};
 
@@ -12,10 +13,24 @@ const Header = (props: Props) => {
   const { user } = useUser();
   const location = useLocation();
   const { userDetail } = useContext(UserDetailContext);
+  const { has } = useAuth();
+  const hasUnlimitedAccess = has && has({ plan: "unlimited" });
+
+  const MenuOptions = [
+    { name: "Workspace", path: "/workspace" },
+    { name: "Pricing", path: "/workspace/pricing" },
+  ];
 
   return (
     <div className="flex items-center justify-between px-10 shadow">
       <img src={logo} alt="Logo" width={130} height={130} />
+      <ul className="flex gap-10">
+        {MenuOptions.map((menu, index) => (
+          <Link to={menu.path} key={index} className="">
+            <h2>{menu.name}</h2>
+          </Link>
+        ))}
+      </ul>
       {!user ? (
         <SignInButton mode="modal">
           <Button>Get Started</Button>
@@ -24,9 +39,11 @@ const Header = (props: Props) => {
         <div className="flex gap-5 items-center">
           <UserButton />
           {location.pathname.includes("/workspace") ? (
-            <div className="flex gap-2 items-center p-2 px-3 bg-orange-100 rounded-full">
-              <Gem /> {userDetail?.credits ?? 0}
-            </div>
+            !hasUnlimitedAccess && (
+              <div className="flex gap-2 items-center p-2 px-3 bg-orange-100 rounded-full">
+                <Gem /> {userDetail?.credits ?? 0}
+              </div>
+            )
           ) : (
             <Button>Go to Workspace</Button>
           )}
